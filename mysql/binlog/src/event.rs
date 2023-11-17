c9b93b3842755e6e25c85bdde70b3368da2c9ec3
fengyang <yueny09@163.com>
2023-11-17 20:04:48 +0800

    EventHeaderFlag 实现

--- mysql/binlog/src/event.rs
+++ mysql/binlog/src/event.rs
@@ -1,29 +1,26 @@
-// use crate::binlog::event_type::EventType;
-//
-//
-// #[derive(Debug, Clone)]
-// #[cfg_attr(feature = "serde", serde::Serialize, serde::DeSerialize)]
-// pub struct EventHeader {
-//     pub timestamp: Int4,
-//     pub event_type: Int1,
-//     pub server_id: Int4,
-//     pub event_size: Int4,
-//     pub log_pos: Int4,
-//     pub flags: EventHeaderFlag,
-// }
-//
-// /// binlog file 内的 event data
-// pub struct BinlogEvent {
-//     /// 事件类型，Format_desc、Query、Table_map、Write_rows、Xid、Update_rows、Delete_rows...
-//     event_type: EventType,
-//
-//     /// 数据的开始 pos, 包含
-//     start_log_pos: i32,
-//
-//     /// 数据的结束 pos, 不包含
-//     end_log_pos : i32,
-//
-//     /// 数据
-//     info : String,
-//
-// }
+use crate::event_header::EventHeader;
+
+/////////////////////////////////////
+///  Event Data
+/////////////////////////////////////
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", serde::Serialize, serde::DeSerialize)]
+pub struct EventRaw {
+    pub header: EventHeader,
+    pub payload: Vec<u8>,
+}
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", serde::Serialize, serde::DeSerialize)]
+pub struct Event<P> {
+    pub header: EventHeader,
+    pub payload: P,
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        assert_eq!(1, 1);
+    }
+}
--- mysql/binlog/src/lib.rs
+++ mysql/binlog/src/lib.rs
@@ -1,1 +1,12 @@
 pub mod event;
+pub mod event_header;
+pub mod b_type;
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        assert_eq!(1, 1);
+        println!("binlog lib test");
+    }
+}
